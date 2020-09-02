d0a5c83890f8280d1dc03632fb81c844418fea26
Gabyblackroses <mgabypaz@hotmail.com>
2020-09-01 23:48:18 -0300

    version1

---
+++ js/check.js
@@ -0,0 +1,15 @@
+var people = [
+    {name: 'Jack', age: 50},
+    {name: 'Michael', age: 9},
+    {name: 'John', age: 40},
+    {name: 'Ann', age: 19},
+    {name: 'Elisabeth', age: 16}
+    ]
+
+    document.write('<h3>Adolescentes</h3>');
+    document.write('<hr>');
+    people.forEach(function (teenager) {
+        if (teenager.age > 10 && teenager.age < 20){ 
+        document.write(('<h4>There are teenagers:</h4>', teenager ,teenager.name + ' edad:'+ teenager.age) + "<br/>");
+        }   
+    })
